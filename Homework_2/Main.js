8c8aafbb91dcab774636498d30b7cbd24da9ad84
The5thWill <54751772+The5thWill@users.noreply.github.com>
2021-01-25 20:15:26 -0500

    Update Main.js

---
+++ Homework_2/Main.js
@@ -0,0 +1,16 @@
+console.log("favorite websites
+1 https://roll20.net/welcome
+2 https://www.netflix.com/browse
+3 https://discord.com/
+");
+
+console.log("favorite games
+1 Rainbow 6 siege
+2 Gmod
+3 Elite Dangerous
+");
+console.log("favorite artists
+1 Hajime Isayama
+2 Sergey Gorshkov
+3 Keith Haring
+");
