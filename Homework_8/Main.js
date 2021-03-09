26cfcc0134464c33b69ee010cb2d5d4e1c576910
The5thWill <54751772+The5thWill@users.noreply.github.com>
2021-03-08 21:05:23 -0500

    HW 8

---
+++ Homework_8/Main.js
@@ -0,0 +1,42 @@
+var Selector = "#Land";
+var all = new Array();
+class Info{
+    constructor(selector, imagePath)
+    {
+        this.selector = selector;
+        this.imagePath = imagePath;
+    }
+    get theSelector(){
+        return this.selector;
+    }
+    get theImagePath(){
+        return this.imagePath;
+    }
+    toString()
+    {
+        return this.selector + ":" + this.imagePath;
+    }
+}
+function initializeArray()
+{
+    var = new Info("#Land", "images/dog.jpg");
+    all.push;
+}
+$(document).ready(function(){
+    initializeArray();
+    console.log(all.length);
+    console.log(all[0].toString());
+    console.log(all[0].theSelector);
+    console.log(all[0].theImagePath);
+    $(all[0].theSelector).attr("src", all[0].theImagePath);
+    $("button").click(function(){
+        $(".stuff").fadeOut();
+        $("#third").toggle();
+           setInterval(moveSquare, 1000);
+        $(all[0].theSelector).fadeOut().fadeIn();
+    });
+});
+function moveSquare()
+{
+    $("#square").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
+}
